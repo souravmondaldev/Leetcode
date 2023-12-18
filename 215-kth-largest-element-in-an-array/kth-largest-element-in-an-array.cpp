aa6db43cb92630ab1f8badacbcb2f7f67d37b177
Sourav Mondal <souravmondal0341@gmail.com>
2023-12-18 14:26:43 +0530

    Time: 119 ms (13.59%) | Memory: 58 MB (36.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    // int findKthLargest(vector<int>& nums, int k) {
-    //     // time O(N.logN) space O(N)
-    //     priority_queue<int> numsStoredInIncreasingOrder;
-
-    //     for(int i = 0; i < nums.size(); i ++){
-    //         numsStoredInIncreasingOrder.push(nums[i]);
-    //     }
-
-    //     for(int i = 0; i < k - 1; i ++){
-    //         numsStoredInIncreasingOrder.pop();
-    //     }
-
-    //     return numsStoredInIncreasingOrder.top();
-    // }
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), greater<int>());
-        return nums[k-1];
+        // time O(N.logN) space O(N)
+        priority_queue<int> numsStoredInIncreasingOrder;
+
+        for(int i = 0; i < nums.size(); i ++){
+            numsStoredInIncreasingOrder.push(-nums[i]);
+            if(numsStoredInIncreasingOrder.size() > k){
+                numsStoredInIncreasingOrder.pop();
+            }
+        }
+
+        return -numsStoredInIncreasingOrder.top();
     }
 };
