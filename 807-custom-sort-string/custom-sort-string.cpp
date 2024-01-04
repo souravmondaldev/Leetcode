1c99bda6d941faedfe1e60cf3ad241c09321ca7b
Sourav Mondal <souravmondal0341@gmail.com>
2024-01-04 21:52:19 +0530

    Time: 3 ms (52.35%) | Memory: 6.7 MB (56.74%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans = "";
+        unordered_map<char, int> mp1, mp2;
+        for(int i = 0; i < s.length(); i ++)
+            mp1[s[i]] ++;
+
+        for(int i = 0; i < order.size(); i ++){
+            if(mp1.find(order[i]) != mp1.end()){
+                while(mp1[order[i]]){
+                    ans += order[i];
+                    mp1[order[i]] --;
+                    mp2[order[i]] ++;
+                }
+            }
+        }
+        for(int i = 0; i < s.length(); i ++){
+            if(mp2.find(s[i]) == mp2.end()){
+                ans += s[i];
+            }
+        }
+
+        return ans;
+    }
+};
