9d96f5f85ceca88fb44d6ae07d729b8ee52dd70d
Sourav Mondal <souravmondal0341@gmail.com>
2024-01-07 22:27:46 +0530

    Time: 23 ms (42.65%) | Memory: 65.5 MB (42.65%) - LeetSync

--- 2870-longest-alternating-subarray/longest-alternating-subarray.cpp
+++ 2870-longest-alternating-subarray/longest-alternating-subarray.cpp
@@ -2,31 +2,59 @@ class Solution {
 public:
     int alternatingSubarray(vector<int>& nums) {
         int maxLen = -1;
-        int currMaxLen = 0;
-        for(int i = 0; i < nums.size() - 1; i ++){
-            bool flag = false;
-            for(int j = i + 1; j < nums.size(); j ++){
-                if(!flag){
-                    if(nums[j] - nums[j-1] == 1){
-                        flag = true;
-                        maxLen = max(maxLen, j - i + 1);
-                    }
-                    else {
-                        break;
+        int n = nums.size(), i = 0, j = 1;
+        bool ch = false;
+        while(j < n){
+            if(!ch){
+                if(nums[j] - nums[j - 1] == 1){
+                    ch = true;
+                    maxLen = max(maxLen, j - i + 1);
+                }
+                else {
+                    i = j;
                     }
                 }
                 else {
-                    if(nums[j] - nums[j-1] == -1){
-                        flag = false;
+                    if(nums[j] - nums[j - 1] == -1){
+                        ch = false;
                         maxLen = max(maxLen, j - i + 1);
                     }
-                    else{
-                        break;
+                    else if(nums[j] - nums[j - 1] == 1){
+                        i = j - 1;
+                        maxLen = max(maxLen, j - i + 1);
+                    }
+                    else {
+                        ch = false;
+                        i = j;
                     }
                 }
-            }
-            
+                j ++;
         }
+        // Time(O(n2))
+        // for(int i = 0; i < nums.size() - 1; i ++){
+        //     bool flag = false;
+        //     for(int j = i + 1; j < nums.size(); j ++){
+        //         if(!flag){
+        //             if(nums[j] - nums[j-1] == 1){
+        //                 flag = true;
+        //                 maxLen = max(maxLen, j - i + 1);
+        //             }
+        //             else {
+        //                 break;
+        //             }
+        //         }
+        //         else {
+        //             if(nums[j] - nums[j-1] == -1){
+        //                 flag = false;
+        //                 maxLen = max(maxLen, j - i + 1);
+        //             }
+        //             else{
+        //                 break;
+        //             }
+        //         }
+        //     }
+            
+        // }
         return maxLen;
     }
 };
