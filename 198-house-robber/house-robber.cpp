f754e2db9b3b2e3f60fc5fb9a76c822e19d586ce
Sourav Mondal <souravmondal0341@gmail.com>
2024-01-21 14:42:33 +0530

    Time: 2 ms (40.56%) | Memory: 9 MB (9.67%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        vector<int> dp(n + 1, 0);
+        dp[1] = nums[0];
+        for(int i = 1; i < n; i ++){
+            dp[i + 1] = max(dp[i - 1] + nums[i], dp[i]);
+        }
+
+        return dp[n];
+    }
+};
