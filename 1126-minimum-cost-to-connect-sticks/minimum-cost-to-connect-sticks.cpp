797b61e8d0a5d0da76dea0444f27baa68aa9915d
Sourav Mondal <souravmondal0341@gmail.com>
2024-01-30 14:42:29 +0530

    Time: 68 ms (38.73%) | Memory: 27.6 MB (46.01%) - LeetSync

---
+++ 1126-minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int connectSticks(vector<int>& sticks) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int totalCost = 0;
+        for(int stick : sticks)
+            pq.push(stick);
+        
+        while(pq.size() > 1){
+            int stick1 = pq.top();
+            pq.pop();
+            int stick2 = pq.top();
+            pq.pop();
+            totalCost += stick1 + stick2;
+            pq.push(stick1 + stick2);
+        }
+        return totalCost;
+    }
+};
