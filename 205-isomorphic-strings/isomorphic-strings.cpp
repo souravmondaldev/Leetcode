196c4f4415cc7fb79a95bd1546ae528833a04189
Sourav Mondal <souravmondal0341@gmail.com>
2024-04-02 21:41:44 +0530

    Time: 6 ms (65.28%) | Memory: 8.2 MB (77.12%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int sI[128] = {0}, tI[128] = {0};
+        for(int i = 0; i < s.size(); i ++){
+            if(sI[s[i]] != tI[t[i]])    return false;
+            sI[s[i]] = tI[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
