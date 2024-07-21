1cd34ad997ce1528a1799f98a823ab0ecd72df7d
Sourav Mondal <souravmondal0341@gmail.com>
2024-07-22 01:04:18 +0530

    Time: 19 ms (16.18%) | Memory: 18.5 MB (12.81%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int N = piles.size();
+        // return true;
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1, -1));
+        return optimalPick(0, N - 1, dp, piles);
+    }
+    int optimalPick(int left, int right, vector<vector<int>> &dp, vector<int>& piles){
+        if(left > right){
+            return 0;
+        }
+        if(dp[left][right] != -1)
+            return dp[left][right];
+        int isAlicePicking = (right - left) % 2;
+        int leftPick = isAlicePicking ? piles[left] : 0;
+        int rightPick = isAlicePicking ? piles[right] : 0;
+        return dp[left][right] = max(
+            optimalPick(left + 1, right, dp, piles) + leftPick,
+            optimalPick(left, right - 1, dp, piles) + rightPick);
     }
 };
