f149c26173cdc2c0dfadb618ce45d433f594654c
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-02 16:37:05 +0530

    Time: 13 ms (40.75%) | Memory: 17.5 MB (28.76%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        //BUCKET SORT O(N) O(N)
+        unordered_map<int, int> counts;
+        int maxFreq = 0;
+        //Store the frequencies inside a Map for O(1) access
+        for(int num : nums){
+            ++ counts[num];
+            maxFreq = max(maxFreq, counts[num]);
+        }
+
+        //vector to store numbers with same frequenices
+
+        vector<vector<int>> freqCount(maxFreq + 1);
+        for(auto &count : counts){
+            freqCount[count.second].push_back(count.first);
+        }
+
+        //get top k elemements based on the count
+        vector<int> ans;
+        for(int i = maxFreq; i > 0 && ans.size() < k; i --){
+            for(int j = 0; j < freqCount[i].size() && ans.size() < k; j ++){
+                ans.push_back(freqCount[i][j]);
+            }
+        }
+        return ans;
+
+    }
+};
