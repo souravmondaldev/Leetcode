3c5008cda8a23a70c944442646a75ddbf5c92068
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-02 17:43:51 +0530

    Time: 288 ms (22.54%) | Memory: 122 MB (80.39%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,17 +1,39 @@
-// class MedianFinder {
-// public:
-//     MedianFinder() {
+class MedianFinder {
+    // maxHeap minHeap num
+    //  2.      0.   2
+    //.         2
+    //.  2
+
+    //               3
+    //.  2,3.        
+    //   2.    3.    
+    //.              5
+    //.  2,5.  3
+    //.  2.    3,5
+    // 2,3.    5.     
+    private:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+public:
+    MedianFinder() {
         
-//     }
+    }
     
-//     void addNum(int num) {
-        
-//     }
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
     
-//     double findMedian() {
-        
-//     }
-// };
+    double findMedian() {
+        return maxHeap.size() > minHeap.size() ? (double)maxHeap.top() : (double)(maxHeap.top() + minHeap.top())/2;
+    }
+};
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
@@ -20,24 +42,25 @@
  * double param_2 = obj->findMedian();
  */
 
-class MedianFinder {
-private:
-    vector<int> nums;
-public:
-    MedianFinder() {
+//Insertion sort O(N) O(N) //PASSED TESTCASES
+// class MedianFinder {
+// private:
+//     vector<int> nums;
+// public:
+//     MedianFinder() {
         
-    }
+//     }
     
-    void addNum(int num) {
-        //insertion sort using binary search lower bound
-        if(nums.empty())
-            nums.push_back(num);
-        else nums.insert(lower_bound(nums.begin(), nums.end(), num), num);
-    }
+//     void addNum(int num) {
+//         //insertion sort using binary search lower bound
+//         if(nums.empty())
+//             nums.push_back(num);
+//         else nums.insert(lower_bound(nums.begin(), nums.end(), num), num);
+//     }
     
-    double findMedian() {
-        // sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return n&1 ? (double)nums[n/2] : (double)(nums[n/2-1] + nums[n/2])/2;
-    }
-};
+//     double findMedian() {
+//         // sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         return n&1 ? (double)nums[n/2] : (double)(nums[n/2-1] + nums[n/2])/2;
+//     }
+// };
