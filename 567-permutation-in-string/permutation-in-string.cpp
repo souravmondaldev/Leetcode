eb48f44c7aec07feb5f70d2ee34e9202cdb8dd2b
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-05 16:13:41 +0530

    Time: 115 ms (15.99%) | Memory: 20.1 MB (16.39%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,14 +1,40 @@
 class Solution {
 public:
+    //O(n2-n1)n1logn1 //new string of size n1 and sortig takes logn1 n1+logn1
+    // bool checkInclusion(string s1, string s2) {
+    //     int n1 = s1.size(), n2 = s2.size();
+    //     if(n2 < n1)
+    //         return false;
+    //     sort(s1.begin(), s1.end());
+    //     for(int i = 0; i <= n2 - n1; i ++){
+    //         string s3 = s2.substr(i, n1);
+    //         sort(s3.begin(), s3.end());
+    //         if(s1 == s3)
+    //             return true;
+    //     }
+    //     return false;
+    // }
+    bool ifPermutation(vector<int>&s1Map, vector<int>&s2Map){
+        for(int i = 0; i < 26; i ++){
+            if(s1Map[i] != s2Map[i])
+                return false;
+        }
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.size(), n2 = s2.size();
         if(n2 < n1)
             return false;
-        sort(s1.begin(), s1.end());
+        vector<int> s1Map(26, 0);
+        for(char ch: s1)
+            s1Map[ch - 'a'] ++;
+        
         for(int i = 0; i <= n2 - n1; i ++){
-            string s3 = s2.substr(i, n1);
-            sort(s3.begin(), s3.end());
-            if(s1 == s3)
+            vector<int> s2Map(26, 0);
+            for(int j = i; j < i + n1; j ++){
+                s2Map[s2[j] - 'a'] ++;
+            }
+            if(ifPermutation(s1Map, s2Map))
                 return true;
         }
         return false;
