a8f8e91b7b9115a8515d27cb91cf764c6b7e8128
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-05 18:21:49 +0530

    Time: 557 ms (19.80%) | Memory: 42.3 MB (20.82%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        for(int i = 0; i < nums.size() && nums[i] <= 0; i ++){
-            if(i == 0 || nums[i] != nums[i - 1]){
-                threeSumFunc(nums, i, ans);
-            }
-        }
-        return ans;
-    }
+//    O(n2) O(logn for sorting)
+    // vector<vector<int>> threeSum(vector<int>& nums) {
+    //     sort(nums.begin(), nums.end());
+    //     vector<vector<int>> ans;
+    //     for(int i = 0; i < nums.size() && nums[i] <= 0; i ++){
+    //         if(i == 0 || nums[i] != nums[i - 1]){
+    //             threeSumFunc(nums, i, ans);
+    //         }
+    //     }
+    //     return ans;
+    // }
     void threeSumFunc(vector<int> &nums, int i, vector<vector<int>> &ans){
         int left = i + 1, right = nums.size() - 1;
         while(left < right){
@@ -24,5 +25,26 @@ public:
                 right --;
             else left ++;
         }
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> res;
+        unordered_set<int> duplicates;
+        unordered_map<int, int> seen;
+        for(int i = 0; i < nums.size(); i ++){
+            if(duplicates.count(nums[i]) == 0){
+                duplicates.insert(nums[i]);
+                for(int j = i + 1; j < nums.size(); j ++){
+                    int complement = - nums[i] - nums[j];
+                    if(seen.find(complement) != seen.end() && seen[complement] == i){
+                        vector<int> triplet = {nums[i], nums[j], complement};
+                        sort(triplet.begin(), triplet.end());
+                        res.insert(triplet);
+                    }
+                    seen[nums[j]] = i;
+                }
+            }
+        } 
+        return vector<vector<int>>(res.begin(), res.end());
     }
 };
