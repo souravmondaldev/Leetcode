dfa45056f98bb98bff8f68024c68eeb6b2575f30
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-05 16:26:02 +0530

    Time: 4 ms (81.50%) | Memory: 8.8 MB (50.93%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -14,6 +14,7 @@ public:
     //     }
     //     return false;
     // }
+    //O((n2-n1)*(n1+26) + n1) o(n2-n1) =~ O(1)
     bool ifPermutation(vector<int>&s1Map, vector<int>&s2Map){
         for(int i = 0; i < 26; i ++){
             if(s1Map[i] != s2Map[i])
@@ -21,22 +22,42 @@ public:
         }
         return true;
     }
+    // bool checkInclusion(string s1, string s2) {
+    //     int n1 = s1.size(), n2 = s2.size();
+    //     if(n2 < n1)
+    //         return false;
+    //     vector<int> s1Map(26, 0);
+    //     for(char ch: s1)
+    //         s1Map[ch - 'a'] ++;
+        
+    //     for(int i = 0; i <= n2 - n1; i ++){
+    //         vector<int> s2Map(26, 0);
+    //         for(int j = i; j < i + n1; j ++){
+    //             s2Map[s2[j] - 'a'] ++;
+    //         }
+    //         if(ifPermutation(s1Map, s2Map))
+    //             return true;
+    //     }
+    //     return false;
+    // }
+
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.size(), n2 = s2.size();
         if(n2 < n1)
             return false;
         vector<int> s1Map(26, 0);
-        for(char ch: s1)
-            s1Map[ch - 'a'] ++;
-        
-        for(int i = 0; i <= n2 - n1; i ++){
-            vector<int> s2Map(26, 0);
-            for(int j = i; j < i + n1; j ++){
-                s2Map[s2[j] - 'a'] ++;
-            }
+        vector<int> s2Map(26, 0);
+        for(int i = 0; i < n1; i ++){
+            s1Map[s1[i] - 'a'] ++;
+            s2Map[s2[i] - 'a'] ++;
+        }
+        for(int i = 0; i < n2 - n1; i ++){
             if(ifPermutation(s1Map, s2Map))
                 return true;
+            s2Map[s2[i + n1] - 'a'] ++;
+            s2Map[s2[i] - 'a'] --;
         }
-        return false;
+        return ifPermutation(s1Map, s2Map);
     }
+
 };
