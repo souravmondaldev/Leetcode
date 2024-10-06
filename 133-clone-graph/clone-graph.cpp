92381bd56e7c987e60c5fcae0fcf43ef7a96e0a0
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-06 17:04:43 +0530

    Time: 4 ms (38.36%) | Memory: 11.5 MB (81.69%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -18,21 +18,50 @@ public:
     }
 };
 */
+// O(N + E) O(N) DFS
+// class Solution {
+// private:
+//     unordered_map<Node*, Node*> visited;
+// public:
+//     Node* cloneGraph(Node* node) {
+//         if(node == nullptr)
+//             return node;
+//         if(visited.count(node))
+//             return visited[node];
+
+//         Node* clonedNode = new Node(node->val);
+//         visited[node] = clonedNode;
+//         for(Node* neighbor: node->neighbors){
+//             clonedNode->neighbors.push_back(cloneGraph(neighbor));
+//         }
+
+//         return visited[node];
+//     }
+// };
 
 class Solution {
 private:
-    unordered_map<Node*, Node*> visited;
+   
 public:
     Node* cloneGraph(Node* node) {
         if(node == nullptr)
             return node;
-        if(visited.count(node))
-            return visited[node];
-
+        unordered_map<Node*, Node*> visited;
+        queue<Node*> q;
+        q.push(node);
         Node* clonedNode = new Node(node->val);
         visited[node] = clonedNode;
-        for(Node* neighbor: node->neighbors){
-            clonedNode->neighbors.push_back(cloneGraph(neighbor));
+        while(!q.empty()){
+            Node* currNode = q.front();
+            q.pop();
+            
+            for(Node* neighbor: currNode->neighbors){
+                if(visited.find(neighbor) == visited.end()){
+                    visited[neighbor] = new  Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                visited[currNode]->neighbors.push_back(visited[neighbor]);
+            }
         }
 
         return visited[node];
