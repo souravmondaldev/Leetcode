e954a02dfb2fb652edafe5dbf4e7780292866cf0
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-06 18:54:42 +0530

    Time: 6 ms (58.71%) | Memory: 17.8 MB (44.91%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -16,6 +16,27 @@ public:
             return 0;
         return max(1 + maxDepthTree(root->left), 1 + maxDepthTree(root->right));
     }
+    int maxDepthTreeBfs(TreeNode* root){
+        if(root == nullptr)
+            return 0;
+        int count = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, count});
+        while(!q.empty()){
+            auto nodePair = q.front();
+            q.pop();
+            TreeNode* node = nodePair.first;
+            int currCount = nodePair.second;
+            count = max(count, currCount);
+            if(node->left){
+                q.push({node->left, count + 1});
+            }
+            if(node->right){
+                q.push({node->right, count + 1});
+            }
+        }
+        return count;
+    }
     int maxDepth(TreeNode* root) {
         return maxDepthTree(root);
     }
