e126e875c5d09fcfbeaf2f50060f77e0c9b059d0
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-06 19:12:48 +0530

    Time: 5 ms (24.66%) | Memory: 12.3 MB (8.67%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
+    bool isSameTreeRecursive(TreeNode* p, TreeNode* q) {
         if(p == nullptr && q == nullptr)
             return true;
         if(p == nullptr && q != nullptr)
@@ -21,5 +21,32 @@ public:
         if(p->val != q->val)
             return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    bool isSameTreeBFS(TreeNode* pNode, TreeNode* qNode){
+        queue<TreeNode*> queue; 
+        queue.push(pNode);
+        queue.push(qNode);
+        while(!queue.empty()){
+            TreeNode* p = queue.front();
+            queue.pop();
+            TreeNode* q = queue.front();
+            queue.pop();
+            if(p == nullptr && q == nullptr)
+                continue;
+            if(p == nullptr && q != nullptr)
+                return false;
+            if(p != nullptr && q == nullptr)
+                return false;
+            if(p->val != q->val)
+                return false;
+            queue.push(p->left);
+            queue.push(q->left);
+            queue.push(p->right);
+            queue.push(q->right);
+        }
+        return true;
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return isSameTreeBFS(p, q);
     }
 };
