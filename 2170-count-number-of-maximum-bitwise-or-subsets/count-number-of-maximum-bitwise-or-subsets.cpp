11486e9f1cc0f39135ef31cf50d9b1ac7032b0fa
Sourav Mondal <souravmondal0341@gmail.com>
2024-10-19 01:11:58 +0530

    Time: 116 ms (12.52%) | Memory: 309.2 MB (8.94%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -5,14 +5,17 @@ public:
         for(int num : nums){
             maxOrValue |= num;
         }
-        return bruteForceSubsets(nums, 0, 0, maxOrValue);
+        vector<vector<int>> memo(nums.size(), vector<int> (maxOrValue + 1, -1));
+        return bruteForceSubsets(nums, 0, 0, maxOrValue, memo);
     }
-    int bruteForceSubsets(vector<int> &nums, int idx , int currentOr, int targetOr){
+    int bruteForceSubsets(vector<int> &nums, int idx , int currentOr, int targetOr, vector<vector<int>>& memo){
         if(idx == nums.size()){
             return currentOr == targetOr ? 1 : 0;
         }
-        int countWithout = bruteForceSubsets(nums, idx + 1, currentOr, targetOr);
-        int countWith = bruteForceSubsets(nums, idx + 1, currentOr | nums[idx], targetOr);
-        return countWith + countWithout;
+        if(memo[idx][currentOr] != -1)
+            return memo[idx][currentOr];
+        int countWithout = bruteForceSubsets(nums, idx + 1, currentOr, targetOr, memo);
+        int countWith = bruteForceSubsets(nums, idx + 1, currentOr | nums[idx], targetOr, memo);
+        return memo[idx][currentOr] = countWith + countWithout;
     }
 };
