9db6fff3298798cf71e0ae25fccecd3335ce90e2
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-09 00:30:55 +0530

    Time: 445 ms (5.06%) | Memory: 124.7 MB (26.13%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,39 +1,39 @@
-class MedianFinder {
-    // maxHeap minHeap num
-    //  2.      0.   2
-    //.         2
-    //.  2
+// class MedianFinder {
+//     // maxHeap minHeap num
+//     //  2.      0.   2
+//     //.         2
+//     //.  2
 
-    //               3
-    //.  2,3.        
-    //   2.    3.    
-    //.              5
-    //.  2,5.  3
-    //.  2.    3,5
-    // 2,3.    5.     
-    private:
-    priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-public:
-    MedianFinder() {
+//     //               3
+//     //.  2,3.        
+//     //   2.    3.    
+//     //.              5
+//     //.  2,5.  3
+//     //.  2.    3,5
+//     // 2,3.    5.     
+//     private:
+//     priority_queue<int> maxHeap;
+//     priority_queue<int, vector<int>, greater<int>> minHeap;
+// public:
+//     MedianFinder() {
         
-    }
+//     }
     
-    void addNum(int num) {
-        maxHeap.push(num);
-        minHeap.push(maxHeap.top());
-        maxHeap.pop();
+//     void addNum(int num) {
+//         maxHeap.push(num);
+//         minHeap.push(maxHeap.top());
+//         maxHeap.pop();
 
-        if(minHeap.size() > maxHeap.size()){
-            maxHeap.push(minHeap.top());
-            minHeap.pop();
-        }
-    }
+//         if(minHeap.size() > maxHeap.size()){
+//             maxHeap.push(minHeap.top());
+//             minHeap.pop();
+//         }
+//     }
     
-    double findMedian() {
-        return maxHeap.size() > minHeap.size() ? (double)maxHeap.top() : (double)(maxHeap.top() + minHeap.top())/2;
-    }
-};
+//     double findMedian() {
+//         return maxHeap.size() > minHeap.size() ? (double)maxHeap.top() : (double)(maxHeap.top() + minHeap.top())/2;
+//     }
+// };
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
@@ -43,24 +43,23 @@ public:
  */
 
 //Insertion sort O(N) O(N) //PASSED TESTCASES
-// class MedianFinder {
-// private:
-//     vector<int> nums;
-// public:
-//     MedianFinder() {
+class MedianFinder {
+private:
+    vector<int> stream;
+public:
+    MedianFinder() {
         
-//     }
+    }
     
-//     void addNum(int num) {
-//         //insertion sort using binary search lower bound
-//         if(nums.empty())
-//             nums.push_back(num);
-//         else nums.insert(lower_bound(nums.begin(), nums.end(), num), num);
-//     }
+    void addNum(int num) {
+        if(stream.size() == 0){
+            stream.push_back(num);
+        }
+        else stream.insert(lower_bound(stream.begin(), stream.end(), num), num);
+    }
     
-//     double findMedian() {
-//         // sort(nums.begin(), nums.end());
-//         int n = nums.size();
-//         return n&1 ? (double)nums[n/2] : (double)(nums[n/2-1] + nums[n/2])/2;
-//     }
-// };
+    double findMedian() {
+        return stream.size() % 2 ? stream[stream.size()/2] :
+            (double)(stream[stream.size()/2 - 1] + stream[stream.size()/2]) / 2;
+    }
+};
