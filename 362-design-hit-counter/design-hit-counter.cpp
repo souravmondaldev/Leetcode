7137671e63835c5f0440f616bb7caaaa71264b70
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-09 01:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (6.14%) - LeetSync

--- 362-design-hit-counter/design-hit-counter.cpp
+++ 362-design-hit-counter/design-hit-counter.cpp
@@ -1,21 +1,55 @@
+// class HitCounter {
+//     queue<int> hits;
+// public:
+//     HitCounter() {
+        
+//     }
+    
+//     void hit(int timestamp) {
+//         hits.push(timestamp);
+//     }
+    
+//     int getHits(int timestamp) {
+//         while(!hits.empty()){
+//             int diff = timestamp - hits.front();
+//             if(diff >= 300) hits.pop();
+//             else break;
+//         }
+//         return hits.size();
+//     }
+// };
+
+// /**
+//  * Your HitCounter object will be instantiated and called as such:
+//  * HitCounter* obj = new HitCounter();
+//  * obj->hit(timestamp);
+//  * int param_2 = obj->getHits(timestamp);
+//  */
 class HitCounter {
-    queue<int> hits;
+    int total = 0;
+    queue<pair<int, int>> hits;
 public:
     HitCounter() {
         
     }
     
     void hit(int timestamp) {
-        hits.push(timestamp);
+        if(hits.empty() || hits.front().first != timestamp)
+            hits.push({timestamp, 1});
+        else hits.front().second ++;
+        total ++;
     }
     
     int getHits(int timestamp) {
         while(!hits.empty()){
-            int diff = timestamp - hits.front();
-            if(diff >= 300) hits.pop();
+            int diff = timestamp - hits.front().first;
+            if(diff >= 300) {
+                total -= hits.front().second;
+                hits.pop();
+            }
             else break;
         }
-        return hits.size();
+        return total;
     }
 };
 
