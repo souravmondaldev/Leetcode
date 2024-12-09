ac708f733120ac2227ec7dbb80ce4172664ca888
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-10 01:58:33 +0530

    Time: 11 ms (77.46%) | Memory: 28.7 MB (32.33%) - LeetSync

---
+++ 218-the-skyline-problem/the-skyline-problem.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<vector<int>> ans;
+        vector<pair<int, int>> heights;
+        multiset<int> pq;
+        pq.insert(0);
+        int prevMax = 0;
+        for(auto building : buildings){
+            int start = building[0];
+            int end = building[1];
+            int height = building[2];
+            heights.push_back({start, -height});
+            heights.push_back({end, height});
+        }
+        sort(heights.begin(), heights.end(), [&](auto a, auto b){
+            if (a.first != b.first) return a.first < b.first;
+            return a.second < b.second;
+        });
+        for (auto& height : heights) {
+            if (height.second < 0) { // Start of building
+                pq.insert(-height.second);
+            } else { // End of building
+                pq.erase(pq.find(height.second));
+            }
+
+            // Current max height
+            int currMax = *pq.rbegin();
+            if (currMax != prevMax) { // Critical point
+                ans.push_back({height.first, currMax});
+                prevMax = currMax;
+            }
+        }
+        return ans;
+        
+    }
+};
