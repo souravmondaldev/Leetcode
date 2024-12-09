2d3499c50cd1a7eb3a5afa815b586f27825c84ad
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-09 16:58:22 +0530

    Time: 385 ms (21.18%) | Memory: 197.1 MB (24.25%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -1,25 +1,54 @@
+// class Solution {
+// public:
+//     int numMatchingSubseq(string s, vector<string>& words) {
+//         int count = 0;
+//         map<string, bool> mp;
+//         for(string word : words){
+//             if(mp.find(word) != mp.end()){
+//                 if(mp[word] == true)
+//                     count ++;
+//                 continue;
+//             }
+//             mp[word] = subseq(s, word);
+//             count += (mp[word] == true);
+//         }
+//         return count;
+//     }
+//     bool subseq(string s, string word){
+//         int idx = 0;
+//         for(char ch : s){
+//             if(idx < word.length() && word[idx] == ch)
+//                 idx ++;
+//         }
+//         return idx == word.length();
+//     }
+// };
+
 class Solution {
 public:
-    int numMatchingSubseq(string s, vector<string>& words) {
-        int count = 0;
-        map<string, bool> mp;
-        for(string word : words){
-            if(mp.find(word) != mp.end()){
-                if(mp[word] == true)
-                    count ++;
-                continue;
+    int numMatchingSubseq(string S, vector<string>& words) {
+        int n_subsequences = 0;
+        unordered_map<char, vector<string>> state;
+
+        // Populate the initial state
+        for (const string& word : words) {
+            state[word[0]].push_back(word.substr(1));
+        }
+
+        // Process each character in the string S
+        for (char ch : S) {
+            vector<string> old_bucket = state[ch];
+            state[ch].clear(); // Clear current bucket as we'll process it
+
+            for (const string& suffix : old_bucket) {
+                if (suffix.empty()) {
+                    n_subsequences++; // Fully matched word
+                } else {
+                    state[suffix[0]].push_back(suffix.substr(1));
+                }
             }
-            mp[word] = subseq(s, word);
-            count += (mp[word] == true);
         }
-        return count;
-    }
-    bool subseq(string s, string word){
-        int idx = 0;
-        for(char ch : s){
-            if(idx < word.length() && word[idx] == ch)
-                idx ++;
-        }
-        return idx == word.length();
+
+        return n_subsequences;
     }
 };
