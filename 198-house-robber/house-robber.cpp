0a490d56b4a93547fe0b62fb16e1a1e34e2c0ef5
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-10 10:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (17.34%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> memo(n + 1, -1);
-        return robOpt(n, nums, memo);
+        int rob1 = 0, rob2 = 0;
+        for(int num : nums){
+            int currRob = max(rob1 + num, rob2);
+            rob1 = rob2;
+            rob2 = currRob;
+        }
+        return max(rob1, rob2);
     }
     int robOpt(int i, vector<int> &nums, vector<int> &memo){
         if(i < 1)
             return 0;
         if(memo[i] != -1)
             return memo[i];
-        return memo[i] = max( robOpt(i - 1, nums, memo),
+        return memo[i] = max(robOpt(i - 1, nums, memo),
                             nums[i - 1] + robOpt(i - 2, nums, memo));
     }
 };
