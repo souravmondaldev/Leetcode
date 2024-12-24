d1aa991de241f6eb737c102a8681126bf3278946
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-24 12:28:01 +0530

    Time: 24 ms (71.96%) | Memory: 27.6 MB (13.69%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,21 +3,28 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
-        int dp[n + 1][m + 1];
-        for(int i = 0; i <= n; i ++){
-            for(int j = 0; j <= m; j ++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = 0;
-                    continue;
+        // int dp[n + 1][m + 1];
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        // return recur(text1, text2, n - 1, m - 1, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for(int i = 0; i < n ; i ++){
+            for(int j = 0; j < m; j ++){
+                if(text1[i] == text2[j]){
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
                 }
-                if(text1[i - 1] == text2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }
-                else{
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-                }
+                else dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
             }
         }
+        return dp[n][m];
+    }
+    int recur(string text1, string text2, int n, int m, vector<vector<int>> &dp){
+        if(n == 0 || m == 0)
+            return 0;
+        if(dp[n][m] != -1)
+            return dp[n][m];
+        if(text1[n] == text2[m])
+            dp[n][m] = recur(text1, text2, n - 1, m - 1, dp) + 1;
+        else dp[n][m] = max(recur(text1, text2, n - 1, m, dp), recur(text1, text2, n, m - 1, dp));
         return dp[n][m];
     }
 };
