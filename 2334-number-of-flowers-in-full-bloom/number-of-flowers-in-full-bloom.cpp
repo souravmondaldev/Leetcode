0363bc8d0a16d944625046e2552fff07c56ac1e6
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-27 22:24:58 +0530

    Time: 87 ms (36.78%) | Memory: 101.2 MB (15.87%) - LeetSync

---
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        map<int, int> bloomOrder;
+        bloomOrder[0] = 0;
+        for(auto flower : flowers){
+            int start = flower[0], end = flower[1] + 1;
+            bloomOrder[start] ++;
+            bloomOrder[end] --;
+        }
+        vector<int> pos, prefix;
+        int curr = 0;
+        for(auto bloom : bloomOrder){
+            pos.push_back(bloom.first);
+            curr += bloom.second;
+            prefix.push_back(curr);
+        }
+        vector<int> ans;
+        for(int person : people){
+            int i = upper_bound(pos.begin(), pos.end(), person) - pos.begin() - 1;
+            ans.push_back(prefix[i]);
+        }
+        return ans;
+
+    }
+};
