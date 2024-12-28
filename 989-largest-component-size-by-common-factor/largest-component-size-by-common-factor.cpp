3eff313590a23577a43e7d3aa5464bd8b51155a7
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-29 00:22:03 +0530

    Time: 155 ms (69.59%) | Memory: 137.3 MB (22.81%) - LeetSync

--- 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.cpp
+++ 989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.cpp
@@ -11,10 +11,10 @@ public:
         int parentU = find(u);
         int parentV = find(v);
         if(parentU != parentV){
-            if(rank[parentU] > rank[parentV])
-                swap(parentU, parentV);
+            // if(rank[parentU] > rank[parentV])
+            //     swap(parentU, parentV);
             parent[parentU] = parentV;
-            rank[parentV] += rank[parentU];
+            // rank[parentV] += rank[parentU];
         }
     }
     int largestComponentSize(vector<int>& nums) {
@@ -26,8 +26,8 @@ public:
         for(int num : nums){
             for(int i = 2; i*i <= num; i ++){
                 if(num % i == 0){
-                    unionDsu(i, num);
-                    unionDsu(i, num/i);
+                    unionDsu(num, i);
+                    unionDsu(num/i, i);
                 }
             }
         }
