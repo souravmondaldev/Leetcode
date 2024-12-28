e26f86f27c1acc0c2d53e63d07da1d9698a982ab
Sourav Mondal <souravmondal0341@gmail.com>
2024-12-28 23:59:03 +0530

    Time: 531 ms (5.02%) | Memory: 26 MB (49.51%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l = 0, r = height.size()-1, level = 0, water = 0;
-        while (l < r) {
-            int lower = height[height[l] < height[r] ? l++ : r--];
-            level = max(level, lower);
-            water += level - lower;
+        int left = 0, right = height.size() - 1;
+        int maxLevel = 0, totalWater = 0;
+
+        while (left < right) {
+            // Determine the lower height side
+            if (height[left] < height[right]) {
+                // Update the water level for the left side
+                maxLevel = max(maxLevel, height[left]);
+                totalWater += maxLevel - height[left];
+                cout << left << " : maxLevel :" << maxLevel << " " << height[left] <<endl;
+                left++; // Move the left pointer
+            } else {
+                // Update the water level for the right side
+                maxLevel = max(maxLevel, height[right]);
+                totalWater += maxLevel - height[right];
+                cout << right << " : maxLevel :" << maxLevel << " " << height[right] <<endl;
+
+                right--; // Move the right pointer
+            }
         }
-        return water;
+
+        return totalWater;
     }
 };
