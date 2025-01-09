2ba11c5d5ccc8484be3582323a16cc0847d25247
Sourav Mondal <souravmondal0341@gmail.com>
2025-01-09 18:17:31 +0530

    Time: 127 ms (87.27%) | Memory: 73.2 MB (43.99%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
+    // SPACE O(BUS * STOPS) due to adjList and Time complexity O(BUS * BUS * STOP)  
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        if(source == target)
-            return 0;
-
-        unordered_map<int, vector<int>> adjList;
-        unordered_set<int> visBus;
-        queue<int> currentBus;
-        for(int bus = 0; bus < routes.size(); bus ++){
-            for(auto stop: routes[bus]){
-                adjList[stop].push_back(bus);
+        if(source == target) return 0;
+        unordered_map<int, vector<int>> routeToBus;
+        for(int i = 0; i < routes.size(); i ++){
+            for(int route : routes[i]){
+                routeToBus[route].push_back(i);
             }
         }
-        for(int bus : adjList[source]){
-            currentBus.push(bus);
-            visBus.insert(bus);
-        }
-        int stopsVisited = 1; 
-        while(!currentBus.empty()){
-            int queuedBus = currentBus.size();
-            while(queuedBus --){
-                int bus = currentBus.front();
-                currentBus.pop();
-                for(int stop : routes[bus]){
-                    if(stop == target){
-                        return stopsVisited;
-                    }
-                    for(int nextBus : adjList[stop]){
-                        if(!visBus.count(nextBus)){
-                            visBus.insert(nextBus);
-                            currentBus.push(nextBus);
+        unordered_set<int> visitedRoute;
+        unordered_set<int> visitedBus;
+        queue<int> q;
+        q.push(source);
+        visitedRoute.insert(source);
+        int level = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz --){
+                int currentRoute = q.front();
+                q.pop();
+                if(currentRoute == target){
+                    return level;
+                }
+                for(int bus : routeToBus[currentRoute]){
+                    if(visitedBus.count(bus))
+                        continue;
+                    visitedBus.insert(bus);
+                    for(int route : routes[bus]){
+                        if(!visitedRoute.count(route)){
+                            visitedRoute.insert(route);
+                            q.push(route);
                         }
                     }
-                    
                 }
             }
-            stopsVisited++;
+            level ++;
         }
         return -1;
     }
