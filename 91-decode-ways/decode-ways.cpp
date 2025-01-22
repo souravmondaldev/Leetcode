25c024c1a6689444b5134d62e2eeda9c71ccbe82
Sourav Mondal <souravmondal0341@gmail.com>
2025-01-22 10:43:46 +0530

    Time: 2 ms (27.93%) | Memory: 10.9 MB (16.44%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,22 +1,23 @@
+
 class Solution {
 public:
-    int numDecodings(string s) {
-         int n = s.length();
-         if(s[0] == '0')
-             return 0;
-         int one_back = 1, two_back = 1;
-         for(int i = 1; i < n; i ++){
-             int curr = 0;
-             if(s[i] != '0')
-                 curr = one_back;
-             int currDig = stoi(s.substr(i-1, 2));
-            //  cout << one_back << " : "<< two_back << " : " << curr << " " << currDig <<endl;
-             if(currDig >= 10 and currDig <= 26)
-                    curr += two_back;
-             two_back = one_back;
-             one_back = curr;
+    int df(string s, int pos, vector<int>& dp) {
+        if (pos == s.size())
+            return 1;
+        if (s[pos] == '0')
+            return 0;
 
-         }
-        return one_back;
+        if (dp[pos] != -1)
+            return dp[pos];
+
+        int count = df(s, pos + 1, dp);
+        if (pos < s.size() - 1 && s.substr(pos, 2) < "27") {
+            count += df(s, pos + 2, dp);
+        }
+        return dp[pos] = count;
+    }
+    int numDecodings(string s) {
+        vector<int> dp(s.size() + 1, -1);
+        return df(s, 0, dp);
     }
 };
