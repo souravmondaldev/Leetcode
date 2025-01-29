8f154aaf732754ab96dd7eac2a989baa01d5df93
Sourav Mondal <souravmondal0341@gmail.com>
2025-01-29 20:56:11 +0530

    Time: 78 ms (45.11%) | Memory: 260.7 MB (58.65%) - LeetSync

---
+++ 1650-find-root-of-n-ary-tree/find-root-of-n-ary-tree.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* findRoot(vector<Node*> tree) {
+        unordered_set<int> seen;
+        for(Node* node : tree){
+            for(Node* childNode : node->children){
+                seen.insert(childNode->val);
+            }
+        }
+        Node* root = nullptr;
+        for(Node* node : tree){
+            if(!seen.count(node->val)){
+                root = node;
+                break;
+            }
+        }
+        return root;
+    }
+};
