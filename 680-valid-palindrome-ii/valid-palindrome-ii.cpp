970e1b6582439ba39b8732b74aa4ab5cd7d8a829
Sourav Mondal <souravmondal0341@gmail.com>
2025-09-30 10:29:33 +0530

    Time: 6 ms (38.77%) | Memory: 26.5 MB (50.47%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,32 +1,23 @@
 class Solution {
-    public:
-    bool checkPalindrome(string s, int i, int j) {
-        while (i < j) {
-            if (s[i] != s[j]) {
+public:
+    bool checkPalindrome(string s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j])
                 return false;
-            }
-            
-            i++;
-            j--;
+            i ++;
+            j --;
         }
-        
         return true;
     }
-    
     bool validPalindrome(string s) {
-        int i = 0;
-        int j = s.length() - 1;
-        
-        while (i < j) {
-            // Found a mismatched pair - try both deletions
-            if (s[i] != s[j]) {
-                return (checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j));
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            if(s[left] != s[right]){
+                return (checkPalindrome(s, left, right - 1) || checkPalindrome(s, left+1, right));
             }
-            
-            i++;
-            j--;
+            left ++;
+            right --;
         }
-        
         return true;
     }
 };
