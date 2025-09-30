dac39b05d39db187d9ab570cf1a38776b4116758
Sourav Mondal <souravmondal0341@gmail.com>
2025-09-30 15:34:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.04%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,7 +4,8 @@ public:
         int sI[128] = {0}, tI[128] = {0};
         for(int i = 0; i < s.size(); i ++){
             if(sI[s[i]] != tI[t[i]])    return false;
-            sI[s[i]] = tI[t[i]] = i + 1;
+            sI[s[i]] = i + 1;
+            tI[t[i]] = i + 1;
         }
         return true;
     }
