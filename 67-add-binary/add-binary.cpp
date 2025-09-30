5dbd53e5c2496cb00925c83275aa435f30433296
Sourav Mondal <souravmondal0341@gmail.com>
2025-09-30 15:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (99.82%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        string result = "";
+        while(i >= 0 || j >= 0 || carry > 0){
+            if(i >= 0){
+                carry += a[i]-'0';
+                i --;
+            }
+            if(j >= 0){
+                carry += b[j]-'0';
+                j --;
+            }
+            result += (carry%2 ? "1" :"0") ;
+            carry /= 2;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
