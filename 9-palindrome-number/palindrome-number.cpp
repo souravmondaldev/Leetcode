6f13f6e29828891865e69349b58b478a5c9e43ae
Sourav Mondal <souravmondal0341@gmail.com>
2025-09-30 12:31:42 +0530

    Time: 1 ms (59.42%) | Memory: 8.6 MB (67.55%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    // O(log10(N)) O(1)
     bool isPalindrome(int x) {
-        string num= to_string(x);
-        int left = 0, right = num.length() - 1;
-        while(left < right){
-            if(num[left] != num[right])
-                return false;
-            left ++;
-            right --;
+        // string num= to_string(x);
+        // int left = 0, right = num.length() - 1;
+        // while(left < right){
+        //     if(num[left] != num[right])
+        //         return false;
+        //     left ++;
+        //     right --;
+        // }
+        // return true;
+        if(x < 0 || (x%10 == 0 && x > 0))
+            return false;
+        int reverse = 0;
+        while(x > reverse){
+            reverse = reverse*10 + x%10 ;
+            x = x/10;
         }
-        return true;
+        return x == reverse || x == reverse / 10; //In case of odd 123 will be output insteadf of 12321
     }
 };
