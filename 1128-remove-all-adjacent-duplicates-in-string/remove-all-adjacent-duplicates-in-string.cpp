c45be35136d579266d4efd3ca51035fc1b1c959f
Sourav Mondal <souravmondal0341@gmail.com>
2025-10-03 18:36:39 +0530

    Time: 3 ms (80.05%) | Memory: 13.7 MB (60.69%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        std::string result = "";
+        for (char ch : s) {
+            if (result.empty() || result.back() != ch)
+                result.push_back(ch);
+            else
+                result.pop_back();
+        }
+        return result;
+    }
+};
