0d78587ecac0901f9c18bbaf70dd0e74e77caf46
Sourav Mondal <souravmondal0341@gmail.com>
2025-10-04 11:43:55 +0530

    Time: 136 ms (73.04%) | Memory: 169.9 MB (89.31%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.cpp
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.cpp
@@ -1,19 +1,34 @@
 class SparseVector {
 public:
-    unordered_map<int, int> array;
+    // unordered_map<int, int> array;
+    vector<pair<int, int>> indexToNum;
+    int binary_search(std::vector<std::pair<int, int>>& index_to_num, int target_index) {
+        int left = 0;
+        int right = index_to_num.size() - 1;
+        while (left <= right) {
+            int mid = (right - left)/2 + left;
+            if (index_to_num[mid].first == target_index)
+                return index_to_num[mid].second;
+            if (index_to_num[mid].first < target_index)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return 0;
+    }
+
     SparseVector(vector<int> &nums) {
         for(int i = 0; i < nums.size(); i ++){
             if(nums[i])
-                array[i] = nums[i];
+                indexToNum.push_back({i, nums[i]});
         }
     }
     
     // Return the dotProduct of two sparse vectors
     int dotProduct(SparseVector& vec) {
         int result = 0;
-        for(auto [idx, num] : array){
-            if(vec.array.find(idx) != vec.array.end())
-                result += num * vec.array[idx];
+        for(auto [idx, num] : indexToNum){
+            result += num * (binary_search(vec.indexToNum, idx));
         }
         return result;
     }
