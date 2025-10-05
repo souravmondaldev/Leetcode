c95d3f085a5be12b0d186d99b4e24cc087af5be5
Sourav Mondal <souravmondal0341@gmail.com>
2025-10-05 21:18:02 +0530

    Time: 3 ms (97.75%) | Memory: 75.6 MB (11.57%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,16 +1,52 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         // time O(N.logk) space O(k)
+//         priority_queue<int, vector<int>, greater<int>> numsStoredInIncreasingOrder;
+
+//         for(int i = 0; i < nums.size(); i ++){
+//             numsStoredInIncreasingOrder.push(nums[i]);
+//             if(numsStoredInIncreasingOrder.size() > k){
+//                 numsStoredInIncreasingOrder.pop();
+//             }
+//         }
+
+//         return numsStoredInIncreasingOrder.top();
+//     }
+// };
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        // time O(N.logk) space O(k)
-        priority_queue<int, vector<int>, greater<int>> numsStoredInIncreasingOrder;
-
-        for(int i = 0; i < nums.size(); i ++){
-            numsStoredInIncreasingOrder.push(nums[i]);
-            if(numsStoredInIncreasingOrder.size() > k){
-                numsStoredInIncreasingOrder.pop();
+        return quickSelect(nums, k);
+    }
+    
+    int quickSelect(vector<int>& nums, int k) {
+        int pivot = nums[rand() % nums.size()];
+        
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
+        
+        for (int num: nums) {
+            if (num > pivot) {
+                left.push_back(num);
+            } else if (num < pivot) {
+                right.push_back(num);
+            } else {
+                mid.push_back(num);
             }
         }
-
-        return numsStoredInIncreasingOrder.top();
+        
+        if (k <= left.size()) {
+            return quickSelect(left, k);
+        }
+        
+        if (left.size() + mid.size() < k) {
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        
+        return pivot;
+        
     }
 };
