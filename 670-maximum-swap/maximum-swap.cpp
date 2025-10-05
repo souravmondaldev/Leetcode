b6404c0a8a7c9175c8a31d6fb57ff186ce9ed648
Sourav Mondal <souravmondal0341@gmail.com>
2025-10-05 09:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (64.88%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     int maximumSwap(int num) {
         int maxNum = num;
-        string numStr = to_string(num);
-        int n = numStr.length();
-        for(int i = 1; i < n; i ++){
-            for(int j = 0; j < i; j ++){
-                swap(numStr[i], numStr[j]);
-                maxNum = max(maxNum, stoi(numStr));
-                swap(numStr[i], numStr[j]);
+        string numsStr = to_string(num);
+        vector<int> rightmostIndex(10, -1);
+        for(int i = 0; i < numsStr.length(); i ++){
+            rightmostIndex[numsStr[i] - '0'] = i;
+        }
+        for(int i = 0; i < numsStr.length(); i ++){
+            for(int j = 9; j > numsStr[i] - '0'; j --){
+                if(rightmostIndex[j] > i){
+                    swap(numsStr[i], numsStr[rightmostIndex[j]]);
+                    return stoi(numsStr);
+                }
             }
         }
+
+        // int n = numStr.length();
+        // for(int i = 1; i < n; i ++){
+        //     for(int j = 0; j < i; j ++){
+        //         swap(numStr[i], numStr[j]);
+        //         maxNum = max(maxNum, stoi(numStr));
+        //         swap(numStr[i], numStr[j]);
+        //     }
+        // }
+        // sort(numStr.begin(), numStr.end(), greater<int>());
         return maxNum;
     }
 };
