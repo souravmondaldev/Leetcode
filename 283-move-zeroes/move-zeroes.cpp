e04bba9aca8f3936e98a42e14a91371cd40b01c0
Sourav Mondal <souravmondal0341@gmail.com>
2025-10-06 14:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -13,7 +13,13 @@ public:
         int swap_index = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
+                // cout << nums[i] << " " << nums[swap_index] << " Order" <<endl;
                 std::swap(nums[i], nums[swap_index]);
+                // for (int j = 0; j < nums.size(); j++) {
+                //     cout << nums[j] <<" ";
+                // }
+                // cout << endl;
+                
                 swap_index++;
             }
         }
